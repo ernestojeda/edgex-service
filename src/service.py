774ba1712ce2669fab4c4a15e2c161acf90df369
Ernesto Ojeda <ernesto.ojeda@intel.com>
2021-04-06 11:51:00 -0700

    feat: new smile function
    Signed-off-by: Ernesto Ojeda <ernesto.ojeda@intel.com>

--- src/service.py
+++ src/service.py
@@ -2,8 +2,12 @@ class Service:
     def hello(self, name):
         print(f'Hello {name}')
 
+    def smile(self, name):
+        print(f"{name}, thanks for the smile")
+
 if __name__ == "__main__":
     print("Starting up your service...")
 
     s = Service()
     s.hello('world')
+    s.smile('Fred')
