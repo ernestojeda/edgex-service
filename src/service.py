edd9ea4909f881b642a1cd994881ab8f56064a44
Ernesto Ojeda <ernesto.ojeda@intel.com>
2021-04-06 12:00:19 -0700

    docs: update documentation for service
    Signed-off-by: Ernesto Ojeda <ernesto.ojeda@intel.com>

--- src/service.py
+++ src/service.py
@@ -1,10 +1,19 @@
 class Service:
+    '''
+    This will say hello
+    '''
     def hello(self, name):
         print(f'Hello {name}')
 
+    '''
+    This function thinks your smile looks great
+    '''
     def smile(self, name):
         print(f"{name}, thanks for the smile")
 
+'''
+This will start the service
+'''
 if __name__ == "__main__":
     print("Starting up your service...")
 
