802b4a12470798cde4d5ab117b3032f4c939457e
Ernesto Ojeda <ernesto.ojeda@intel.com>
2021-04-06 12:14:30 -0700

    feat: Ireleand Release
    * feat: new smile function
    * docs: update documentation for service
    * fix: update service messaging
    
    Signed-off-by: Ernesto Ojeda <ernesto.ojeda@intel.com>

--- src/service.py
+++ src/service.py
@@ -1,9 +1,22 @@
 class Service:
+    '''
+    This will say hello
+    '''
     def hello(self, name):
         print(f'Hello {name}')
 
+    '''
+    This function thinks your smile looks great
+    '''
+    def smile(self, name):
+        print(f"{name}, your smile is great")
+
+'''
+This will start the service
+'''
 if __name__ == "__main__":
     print("Starting up your service...")
 
     s = Service()
     s.hello('world')
+    s.smile('Fred')
